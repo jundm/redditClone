f81e418bf4f4ee031677e3e5d94b4827eedc1038
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-02 20:22:36 +0900

    create project & setting;

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+let PORT = 4000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (_, res) => res.send("running"));
+
+app.listen(PORT, async () => {
+    console.log(`
+    ###########################################
+    Server running at http://localhost:${PORT}
+    and http://127.0.0.1:${PORT}
+    ###########################################
+    `);
+});

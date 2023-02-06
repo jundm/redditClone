248bb91f8cb47726dbb7a115328900f6999de846
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-06 18:59:51 +0900

    add signup ui;

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,34 @@
+import type {NextPage} from 'next';
+import Link from "next/link";
+
+interface RegisterProps {
+
+}
+
+const Register: NextPage = () => {
+
+    return (
+        <div className="bg-white">
+            <div className="flex flex-col items-center justify-center h-screen p-6">
+                <div className="w-10/12 mx-auto md:w-96">
+                    <h1 className="mb-2 text-lg font-medium">회원가입</h1>
+                    <form action="">
+
+                        <button
+                            className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+                            회원가입
+                        </button>
+                    </form>
+                    <small>
+                        이미 가입하셨나요?
+                        <Link href="/login">
+                            <a className="ml-1 text-blue-500 uppercase">로그인</a>
+                        </Link>
+                    </small>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;

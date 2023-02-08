961a959dfc36af43982fba274e3e93d2b25aff5d
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-08 23:06:58 +0900

    add Context2;

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -1,10 +1,15 @@
 import {User} from "../types";
-import {createContext, ReactNode} from "react";
+import {createContext, ReactNode, useReducer} from "react";
 
 interface State {
     authenticated: boolean;
     user: User | undefined;
     loading: boolean;
+}
+
+interface Action {
+    type: string;
+    payload: any;
 }
 
 const StateContext = createContext<State>({
@@ -15,7 +20,36 @@ const StateContext = createContext<State>({
 
 const DispatchContext = createContext<any>(null);
 
+
+const reducer = (state: State, {type, payload}: Action) => {
+    switch (type) {
+        case "LOGIN":
+            return {
+                ...state,
+                authenticated: true,
+                user: payload
+            };
+        case "LOGOUT":
+            return {
+                ...state,
+                authenticated: false,
+                user: null
+            };
+        case "STOP_LOADING":
+            return {
+                ...state,
+                loading: false
+            };
+        default:
+            throw new Error(`Unknown action type: ${type}`);
+    }
+};
 export const AuthProvider = ({children}: { children: ReactNode }) => {
+    const [state, dispatch] = useReducer(reducer, {
+        user: null,
+        authenticated: false,
+        loading: true
+    });
     return (
         <DispatchContext.Provider value={dispatch}>
             <StateContext.Provider value={state}>{children}</StateContext.Provider>

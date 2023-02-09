bac62885d139e070de6cc2e632e7549af7b63ce1
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-09 17:14:08 +0900

    add subs create user,auth middeleware;

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,24 +1,17 @@
 import {Router, Request, Response} from "express";
 import jwt from "jsonwebtoken";
 import User from "@entities/User";
+import userMiddleware from "@middlewares/user";
+import authMiddleware from "@middlewares/auth";
 
 const createSubs = async (req: Request, res: Response, next: any) => {
     const {name, title, description} = req.body;
 
-    const token = req.cookies.token;
-    if(!token) return next();
 
-    // @ts-ignore
-    const {username} = jwt.verify(token, process.env.JWT_SECRET as string);
-
-    // @ts-ignore
-    const user = await User.findOne({username});
-
-    if(!user) throw new Error
 };
 
 const router = Router();
 
-router.post("/", createSubs);
+router.post("/", userMiddleware, createSubs);
 
 export default router;

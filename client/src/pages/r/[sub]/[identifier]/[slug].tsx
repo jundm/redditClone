912af3420bd1d6a2fdbd2d487ca27d0b7dc48d31
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-12 23:52:52 +0900

    add comment;

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
 import {useRouter} from "next/router";
 import axios from "axios";
 import useSWR from "swr";
 import Link from "next/link";
 import {Post} from "@types";
 import dayjs from "dayjs";
+import {useAuthState} from "@context/auth";
 
 interface PostPageProps {
 
@@ -13,8 +14,21 @@ interface PostPageProps {
 const PostPage = () => {
     const router = useRouter();
     const {identifier, sub, slug} = router.query;
-
+    const {authenticated, user} = useAuthState();
+    const [newComment, setNewComment] = useState("");
     const {data: post, error} = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null);
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        if (newComment.trim() === "") return;
+        try {
+            await axios.post(`/posts/${post?.identifier}/${post?.slug}/comments`, {
+                body: newComment
+            });
+            setNewComment("");
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
         <div className="flex max-w-5xl px-4 pt-5 mx-auto">
             <div className="w-full md:mr-3 md:w-8/12">
@@ -49,6 +63,53 @@ const PostPage = () => {
                                             </span>
                                         </button>
                                     </div>
+                                </div>
+                            </div>
+                            <div>
+                                {/*  댓글 작성 구간  */}
+                                <div className="pr-6 mb-4">{authenticated ? (
+                                    <div>
+                                        <p className="mb-1 text-xs">
+                                            <Link href={`/u/${user?.username}`}>
+                                                <a className="font-semibold text-blue-500">
+                                                    {user?.username}
+                                                </a>
+                                            </Link>
+                                            {" "}으로 댓글 작성
+                                        </p>
+                                        <form onSubmit={handleSubmit}>
+                                            <textarea
+                                                className="w-full p-3 border-gray-300 rounded focus:outline-none focus:border-gray-600"
+                                                onChange={e => setNewComment(e.target.value)}
+                                                value={newComment}
+                                            >
+
+                                            </textarea>
+                                            <div className="flex justify-end">
+                                                <button
+                                                    className="px-3 py-1 text-white bg-gray-400 rounded"
+                                                    disabled={newComment.trim() === ""}
+                                                >
+                                                    댓글 작성
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                ) : (
+                                    <div className="flex items-center justify-between px-2 py-4 border border-gray-200">
+                                        <p className="font-semibold text-gray-400">
+                                            댓글 작성을 위해서 로그인 해주세요.
+                                        </p>
+                                        <div>
+                                            <Link href={`/login`}>
+                                                <a className="px-3 py-1 text-white bg-gray-400 rounded">
+                                                    로그인
+                                                </a>
+
+                                            </Link>
+                                        </div>
+                                    </div>)
+                                }
                                 </div>
                             </div>
                         </>
